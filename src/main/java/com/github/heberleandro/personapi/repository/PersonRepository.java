bfa4dc92a2d1ae336788f6de077afc7d57d3d34d
Doriuntas Barnes <dbarnes369@bears.shawu.edu>
2023-03-29 15:32:07 -0400

    refactor

--- src/main/java/com/github/heberleandro/personapi/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.heberleandro.personapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.github.heberleandro.personapi.entities.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long>{
-}
