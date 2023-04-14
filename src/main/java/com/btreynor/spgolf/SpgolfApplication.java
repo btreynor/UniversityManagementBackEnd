f2ad7751a9bcb210e8a5e8af06c283713efd96d1
Jefferson <roddrick.jefferson@shawu.edu>
2023-04-14 15:30:25 -0400

    working on hibernate

--- src/main/java/com/btreynor/spgolf/SpgolfApplication.java
+++ src/main/java/com/btreynor/spgolf/SpgolfApplication.java
@@ -2,9 +2,14 @@ package com.btreynor.spgolf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.btreynor.spgolf.model"})
+@EnableJpaRepositories(basePackages = {"com.btreynor.spgolf.repository"})
 public class SpgolfApplication  {
 
 	public static void main(String[] args) {
