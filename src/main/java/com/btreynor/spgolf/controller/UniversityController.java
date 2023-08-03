5de313aae9ce62f2e6af4aefeecb2177685d360c
btreynor8719 <btreynor213@bears.shawu.edu>
2023-08-03 18:42:20 -0400

    initial commit

--- src/main/java/com/btreynor/spgolf/controller/UniversityController.java
+++ src/main/java/com/btreynor/spgolf/controller/UniversityController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+
 @RestController
+@CrossOrigin("*")
 public class UniversityController {
 
     @Autowired
